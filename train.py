1c1b28d006ff03f81aae5a6e2ac6708ea115288d
CHJ <gkswns3708@naver.com>
2024-10-02 15:18:16 +0900

    Add annotation at train.py

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import time
 import gc
 import nibabel as nib
 import tqdm as tqdm
+from utils.utils import *
 from utils.meter import AverageMeter
 from utils.general import save_checkpoint, load_pretrained_model, resume_training
 from brats import get_datasets
@@ -46,7 +47,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 os.makedirs("logger", exist_ok=True)
-file_handler = logging.FileHandler(filename="logger/train.log")                           #  로그 메시지를 파일(logger/train_logger.log)에 기록하는 로그 메시지를 기록 하는 Handler
+file_handler = logging.FileHandler(filename="logger/train.log")                           # 로그 메시지를 파일(logger/train_logger.log)에 기록하는 로그 메시지를 기록 하는 Handler
 stream_handler = logging.StreamHandler()                                                  # 콘솔에 로그 메시지를 출력하는 Handler
 fomatter = logging.Formatter(fmt="%(asctime)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S") # 로그 메시지의 출력 형식을 지정하는 Formatter
 file_handler.setFormatter(fomatter)                                                       # 로그 메시지를 파일에 기록하는 Handler
@@ -56,6 +57,49 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
 class Solver:
-    """list of optimizers for training Nerual Networks"""
-    def __init__(self, )
+    """list of optimizers for training NN"""
+    # TODO: lr_scheduler 추가
+    def __init__(self, model: nn.Module, lr: float = 1e-4, weight_decay: float = 1e-5):
+        self.lr = lr
+        self.weight_decay = weight_decay
+
+        self.all_solvers = {
+            "Adam": torch.optim.Adam(model.parameters(), lr=self.lr, 
+                                     weight_decay= self.weight_decay, 
+                                     amsgrad=True), 
+            "AdamW": torch.optim.AdamW(model.parameters(), lr=self.lr, 
+                                     weight_decay= self.weight_decay, 
+                                     amsgrad=True),
+            "SGD": torch.optim.SGD(model.parameters(), lr=self.lr, 
+                                     weight_decay= self.weight_decay),
+        }
+    def select_solver(self, name):
+        return self.all_solvers[name]
+    
+def train_epoch(model, loader, optimizer, loss_func):
+    """
+    Train the model for epoch on MRI image and given ground truth labels using set of arguments
+
+    Parameters
+    ----------
+    model : nn.Module
+        model to train
+        models: UNet3d, ResUnetPlusPlus, SegResNet, VNet, AttentionUnet, UNETR, nnFormer, UXNET
+        # TODO: 다른 model 추가
+    loader : torch.utils.data.Dataset
+        dataset loader
+    optimizer : torch.optim.Optimizer
+        optimizer for training
+        optimizers: Adam, AdamW, SGD 
+        # TODO: 다른 optimizer 추가
+    loss_func : torch.nn.Module
+        loss function for training
+        loss_funcs: `DiceLoss` from monai.losses
+        # TODO: 다른 loss function 추가
+    epoch: int
+        Total training epochs
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    augmenter = DataAugmenter().to(device)
+    
 
